5c694ab4fe63dda10ec1a1aceb31fdf2603cd41d
GOD07thala <sushanthytgaming@gmail.com>
2025-05-27 10:09:26 +0530

    Update Camp.html

--- Camp.php
+++ Camp.php
@@ -1,109 +1,97 @@
-<>
-    <!DOCTYPE html>
-    <html lang="en">
-    <title>Tech Campaign</title>
-    <meta name="robots" content="noindex, follow">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" type="image/png" href="https://barnwaljitechnical.com/wp-content/uploads/2021/12/cropped-download-removebg-preview.png" />
-    <link rel="stylesheet" href="barnwal.css">
-    <link rel="stylesheet" href="styles.css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap" rel="stylesheet">
-    </head>
-    <!-- <style>
-        #telegram-image {
-            animation: continue 2s infinite;
-            animation-direction: alternate;
-        }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Naruto Campaign</title>
+  <meta name="robots" content="noindex, follow">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" type="image/png" href="https://barnwaljitechnical.com/wp-content/uploads/2021/12/cropped-download-removebg-preview.png" />
+  <link rel="stylesheet" href="barnwal.css">
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap" rel="stylesheet">
+</head>
 
-        @keyframes continue {
-            0% {
-                opacity: 0;
-            }
+<body>
+  <section class="promo-main-section">
+    <div class="container">
+      <div class="promo-banner">
+        <div class="promo-banner-content text-white position-relative text-center">
+          <div class="container">
+            <div class="pro-banner-head">
+              <h2><b>Just Follow And Complete Campaign<br>Get ₹100 In Paytm</b></h2>
+              <h2>Complete Campaign & Get 100 Rs</h2>
+            </div>
+          </div>
+        </div>
+      </div>
 
-            100% {
-                opacity: 1;
-            }
-        }
-
-        * {
-            font-family: 'Libre Baskerville', serif;
-        }
-    </style> -->
-
-    <body>
-        <section class="promo-main-section">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 col-md-9">
+          <div class="promo-offer-block rounded bg-white mb-4">
+            <center>
+              <h3 class="promo-block-title"><b>Cashback Will Credit In 24 Hours. Thank You!</b></h3>
+            </center>
             <div class="container">
-                <div class="promo-banner">
-                    <div class="promo-banner-content text-white position-relative text-center">
-                        <div class="container">
-                            <div class="pro-banner-head">
-                                <h2>
-                                    <font size='5.2px'><b>Just Follow And Complete Campagin <br>Get ₹100 In Paytm </b></font>
-                                </h2>
-                                <h2>
-                                    <font size='3.2px'>Complete Campagin & Get 100 Rs</font>
-                                </h2>
-                            </div>
-                        </div>
+              <form action="must.php" method="POST"><br>
+                <div class="form-group row align-items-center mb-4 mt-2">
+                  <div class="col-lg-8">
+                    <div class="input-group promo-input">
+                      <input type="text" required id="number" name="number" class="form-control" maxlength="10" minlength="10" placeholder="Enter Your Paytm Number">
+                      <div class="input-group-append">
+                        <button class="input-group-text" type="submit">Submit</button>
+                      </div>
                     </div>
+                  </div>
                 </div>
-                <div class="row justify-content-center">
-                    <div class="col-lg-8 col-md-9">
-                        <div class="promo-offer-block rounded bg-white mb-4">
-                            <center>
-                                <h3 class="promo-block-title"><b>
-                                        <font size='2px'>Cashback Will Credit In 24 Hours. Thank You!</font>
-                                    </b></h3>
-                            </center>
-                            <div class="container">
-                                <form action="must.php" method="POST"> <br>
-                                    <div class="form-group row align-items-center mb-4 mt-2">
-                                        <div class="col-lg-8">
-                                            <div class="input-group promo-input">
-                                                <input type="hidden" name="refer" value=<?php echo base64_decode($_GET["id"]); ?>>
-                                                <input type="text" required id="number" name="number" class="form-control" maxlength="10" minlength="10" placeholder="Enter Your Paytm Number">
-                                                <div class="input-group-append"> <button class="input-group-text" for="number" type="submit">Submit</button> </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="promo-hr">
-                                        <div class="promo-hr-icon"></div>
-                                    </div>
 
-                                    <script type="text/javascript">(function() {var script=document.createElement("script");script.type="text/javascript";script.async =true;script.src="//telegram.im/widget-button/index.php?id=@EarnRupeesOfficial";document.getElementsByTagName("head")[0].appendChild(script);})();</script>
-<a href="https://telegram.me/barnwaljitechnical" target="_blank" class="telegramim_button telegramim_shadow telegramim_pulse" style="font-size:18px;width:autopx;background:#27A5E7;box-shadow:1px 1px 5px #27A5E7;color:#FFFFFF;border-radius:55px;" title=""><i></i> Join Our Channel </a>
+                <div class="promo-hr">
+                  <div class="promo-hr-icon"></div>
+                </div>
 
-                                            <div class="promo-list">
-                                                <p><span style="font-weight: bold;"> 1. Enter Your PayTM Number And Click Submit<br>
-                                                2.Redirect To Play store<br>
-                                                3. Need Pan Number + Aadhar Number<br>
-                                                4. Enter Aadhar Number and Aadhar Otp To Complete Account<br>
-                                                5.When You Will See Account Number and Ifsc Code Then Your Account Ready<br>
-                                                6.Done!! You Will Get Rs.100 Paytm Cash 👍 🔥<br>⚠️ 
-                                                <span>Important - Must complete KYC & Open Bank Accont<br><br></span>
-                                                        <br> </span><br></p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="mb-4">
-                                        <h5 class="font-weight-bold mb-3">Offer Terms & Conditions :</h5>
-                                        <div class="rounded bg-light text-muted p-3">
-                                            <div class="promo-list">
-                                                <p><span style="color: Red; font-family: Times; font-size: 16px; font-weight: bold;">1. Valid Once Per Unique IP Address,Device, Multiple Registration From Same IP Address,Device(Smartphone) Will not be Paid. <br> 2. use 1 device 1 time only </span><br></p>
-                                            </div>
-                                        </div>
-                                    </div>
-                            </div>
-                        </div>
+                <a href="https://t.me/TEAM7PVB" target="_blank" class="telegramim_button telegramim_shadow telegramim_pulse" style="font-size:18px;width:auto;background:#27A5E7;box-shadow:1px 1px 5px #27A5E7;color:#FFFFFF;border-radius:55px;">Join Our Channel</a>
+
+                <div class="promo-list">
+                  <p>
+                    <strong>
+                      1. Enter Your PayTM Number And Click Submit<br>
+                      2. Redirect To Play Store<br>
+                      3. Need Pan Number + Aadhar Number<br>
+                      4. Enter Aadhar Number and Aadhar OTP To Complete Account<br>
+                      5. When You See Account Number and IFSC Code, Your Account Is Ready<br>
+                      6. Done!! You Will Get Rs.100 Paytm Cash<br>⚠️
+                      <br>Important - Must Complete KYC & Open Bank Account
+                    </strong>
+                  </p>
+                </div>
+
+                <div class="mb-4">
+                  <h5 class="font-weight-bold mb-3">Offer Terms & Conditions:</h5>
+                  <div class="rounded bg-light text-muted p-3">
+                    <div class="promo-list">
+                      <p style="color: Red; font-family: Times; font-size: 16px; font-weight: bold;">
+                        1. Valid Once Per Unique IP Address/Device. Multiple Registrations From Same Device/IP Will Not Be Paid.<br>
+                        2. Use One Device Only Once.
+                      </p>
                     </div>
+                  </div>
                 </div>
+
+              </form>
             </div>
-        </section>
-    </body>
-    <div class="col-md-12" style="position:fixed;bottom: 0;width: 100%;background: -webkit-linear-gradient(right, #de1afa,#26b9e4);right: 0;z-index:99;"><a href="http://telegram.dog/barnwaljitechnical" style="text-decoration:underline;text-decoration-color:#fff;font-family: Poppins-Bold;" target="_blank">
-            <div align="center" style="color: white;font-size:20px;    padding: 5px;"><img src="https://TechAditya.xyz/Influenz/Tech.png" style="width: 35px; margin-right: 5px;"> Join Offical Channel</h3>
-            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
+  </section>
 
-    </html>
+  <div class="col-md-12" style="position:fixed;bottom: 0;width: 100%;background: -webkit-linear-gradient(right, #de1afa,#26b9e4);z-index:99;">
+    <a href="https://t.me/TEAM7PVB" style="text-decoration:underline;color:#fff;font-family: Poppins-Bold;" target="_blank">
+      <div align="center" style="color: white;font-size:20px;padding: 5px;">
+        <img src="https://TechAditya.xyz/Influenz/Tech.png" style="width: 35px; margin-right: 5px;">
+        Join Official Channel
+      </div>
+    </a>
+  </div>
+</body>
+</html>
